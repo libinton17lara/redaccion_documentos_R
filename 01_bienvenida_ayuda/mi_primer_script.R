12c91b3053e0c3c585be1d919347afed74fdbb02
libinton17lara <lara.libinton@gmail.com>
2022-06-16 12:31:41 -0500

    Mi material de la clase 1

---
+++ 01_bienvenida_ayuda/mi_primer_script.R
@@ -0,0 +1,27 @@
+# este es mi primer archivo de R
+# mi nombre es: Libinton Lara
+# Fecha 04-06-2022
+help.start()
+help.search("joint distribution")
+help.search("linear model")
+R.version
+ls()
+#este es mi primer vector 
+edades <-c(54,33,17,90)
+edades <-c(54L,33L,17L,90L)
+class(edades)
+length  # muestra el numero de elementos que tiene el vector
+nombres<-c("alex, wladimir,diego,libinton")
+attributes(nombres)
+# un vector de cada tipo character, numeric, integer, logical
+# Luego por pares detipos van a unir con c[]
+# fhhf
+v1<-c(2,3,4,5,6,7)
+v2<-c(34L, 35L, 23L, 18L)
+v3<-c(TRUE, TRUE, FALSE, FALSE)
+v4<-c("Quito", "Guayaquil","Ambato","cuenca")
+vector_1<-c(v1,v4)
+vector_2<-c(v1,v3)
+vector_3<-c(v2,v4)
+class(vector_2)
+
